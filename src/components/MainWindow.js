2bbe6735d2f6238aa5752ab348e41e57db2ea3f3
Martin Crabtree <mcrabtr33@gmail.com>
2020-02-20 16:28:27 -0500

    playing around with formatting

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -1,6 +1,6 @@
 import React, {useState, useContext, useEffect} from 'react';
 import {Context} from '../Store';
-import {LineChart, Line, YAxis, CartesianGrid, XAxis} from 'recharts';
+import {LineChart, BarChart, Bar, Line, YAxis, CartesianGrid, XAxis} from 'recharts';
 import FilterContainer from './FilterContainer';
 
 
@@ -56,15 +56,26 @@ const MainWindow=()=>{
 
     };
 
+    const renderCustomBarLabel = ({ payload, x, y, width, height, value }) => {
+        return <text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`value: ${value}`}</text>;
+      };
+
     return(
         <section>
             <h4>Main Window</h4>
-            <LineChart width={400} height={400} data={ handleFilterChange() }>
+            <LineChart width={400} height={400} data={ handleFilterChange() }  margin={{ top: 5, right: 20, bottom: 5, left: 0 }} >
                 <Line type="monotone" dataKey="count_female" stroke="#000000" />
-                <CartesianGrid />
-                <XAxis />
-                <YAxis />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="jurisdiction_name" />
+                <YAxis type="number" domain={[0,75]} />
             </LineChart>
+
+            <BarChart width={400} height={400} data={ handleFilterChange() }  margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Bar type="monotone" dataKey="count_female" barSize={30} fill="#8884d8" label={renderCustomBarLabel} />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5"  />
+                <XAxis dataKey="jurisdiction_name" />
+                <YAxis type="number" domain={[0,75]} />
+            </BarChart>
 
             <FilterContainer />
         </section>
