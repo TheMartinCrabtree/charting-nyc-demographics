9d9fd570f611943d02a253ea93ba7fe705263f0b
Martin Crabtree <mcrabtr33@gmail.com>
2020-02-19 19:19:12 -0500

    displaying basic graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {LineChart, Line} from 'recharts';
 import './App.css';
 
+import data from './data';
+// const data = [
+//   {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+//   {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+//   {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
+//   {name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+
+
 function App() {
+  
+  console.log("what is data: ", data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <header><h3>Charting NYC Demographic Data</h3></header>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="count_female" stroke="#000000" />
+      </LineChart>
+
+    </main>
   );
 }
 
